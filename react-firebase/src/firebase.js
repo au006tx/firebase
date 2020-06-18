a84a1c65d46c94b2dd3446ca2a85df2c79e7012d
Salman Ahmed <salman@Salmans-Macbook-Pro-2.local>
2020-06-18 15:35:39 +0530

    initial commit

---
+++ react-firebase/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDGxYRBFMKDZie9nPXr-ROijATGTI3QTGA',
+  authDomain: 'quickstart-1567408826818.firebaseapp.com',
+  databaseURL: 'https://quickstart-1567408826818.firebaseio.com',
+  projectId: 'quickstart-1567408826818',
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
